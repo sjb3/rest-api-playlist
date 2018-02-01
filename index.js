de6587880af702c21f5479068e1b562219111783
Sung J Byun <sungjbyun@D-173-250-249-98.dhcp4.washington.edu>
2018-01-31 20:45:03 -0800

    issue with mongodb

---
+++ index.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+// set up express app
+const app = express();
+
+// connect to mongodb
+mongoose.connect('mongodb://localhost/db');
+mongoose.Promise = global.Promise;
+
+app.use(express.static('public'));
+
+// use body-parser middleware
+app.use(bodyParser.json());
+
+// initialize routes
+app.use('/api', require('./routes/api'));
+
+// listen for requests
+app.listen(process.env.port || 4000, function(){
+    console.log('now listening for requests');
+});
+
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const mongoose = require('mongoose');
+//
+// const mongoURI = process.env.MONGO_URI || 'mongodb://localhost/db';
+//
+// // set up express app
+// const app = express();
+//
+// // connect to mongodb
+// mongoose.connect(mongoURI)
+// mongoose.Promise = global.Promise;
+//
+// app.use(bodyParser.json());
+//
+// // initialize routes
+// app.use('/api', require('./routes/api'))
+//
+// // error handling middleware
+// app.use( (err, req, res, next) => {
+//   // console.log(err);
+//   res.status(422).send({error: err.message})
+// });
+//
+// // listen for req
+// app.listen(process.env.port || 4000, (req, res) => {
+//   console.log('Server up and running');
+// });
